62dbafe032c05a1a8d065617e015a6699c61f773
James Ma <jamesma2324@berkeley.edu>
2025-03-23 13:13:35 -0700

    Version Optimization 9 Photo problem

--- src/public/server.js
+++ src/public/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     next();
 });
 
-
+app.use("/photos", express.static(path.join(__dirname, "uploads")));
 // Middleware
 app.use(bodyParser.json());
 app.use((req, res, next) => {
